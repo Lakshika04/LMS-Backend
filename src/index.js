e9a7f76f2c114eb8410ef9b9ea0e7510f7bf5a59
Lakshika04 <krishikaa204@gmail.com>
2025-11-30 23:10:44 +0530

    allowed all origins

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ import progressRouter from './routes/progress.route.js';
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
